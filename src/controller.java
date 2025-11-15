7bbbe1d50211d2b19996d74a01025466194482c2
Fateh kabir <fatehkabir2000@gmail.com>
2025-11-15 16:38:47 +0600

    set homescreen allignment to center

--- src/controller.java
+++ src/controller.java
@@ -1,14 +1,24 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class controller {
 
     @FXML
     private Button start_GPA_Calculator;
 
-    public void start_GPA_Calculation(ActionEvent event) {
-
+    public void start_GPA_Calculation(ActionEvent event) throws IOException {
+        FXMLLoader loader=new FXMLLoader(getClass().getResource("courseentryscreen.fxml"));
+        Parent root=loader.load();
+        Stage stage=(Stage)((Button)event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 
 }
